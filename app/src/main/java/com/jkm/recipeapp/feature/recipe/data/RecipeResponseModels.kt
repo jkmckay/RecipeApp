3018b033743aa95d1f2ee8794ad7a1f61bf3cd8b
jkm <james.mckay133@gmail.com>
2026-03-09 15:27:39 +1100

    Initial pass of modelling

---
+++ app/src/main/java/com/jkm/recipeapp/feature/recipe/data/RecipeResponseModels.kt
@@ -0,0 +1,38 @@
+package com.jkm.recipeapp.feature.recipe.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+// TODO: Do you need to be a class or is a typealias fine?
+@Serializable
+data class RecipeResponse(
+    @SerialName("recipes") val recipes: List<RecipeDto>? = null,
+)
+
+@Serializable
+data class RecipeDto(
+    @SerialName("dynamicTitle") val title: String? = null,
+    @SerialName("dynamicDescription") val description: String? = null,
+    @SerialName("dynamicThumbnail") val thumbnailUrl: String? = null,
+    @SerialName("dynamicThumbnailAlt") val thumbnailAlt: String? = null,
+    @SerialName("recipeDetails") val details: RecipeDetailsDto? = null,
+    @SerialName("ingredients") val ingredients: List<IngredientDto>? = null,
+)
+
+@Serializable
+data class RecipeDetailsDto(
+    @SerialName("amountLabel") val amountLabel: String? = null,
+    @SerialName("amountNumber") val amountNumber: String? = null,
+    @SerialName("prepLabel") val prepLabel: String? = null,
+    @SerialName("prepNote") val prepNote: String? = null,
+    @SerialName("prepTime") val prepTime: String? = null,
+    @SerialName("cookingLabel") val cookingLabel: String? = null,
+    @SerialName("cookingTime") val cookingTime: String? = null,
+    @SerialName("cookTimeAsMinutes") val cookTimeMinutes: String? = null,
+    @SerialName("prepTimeAsMinutes") val prepTimeMinutes: String? = null,
+)
+
+@Serializable
+data class IngredientDto(
+    @SerialName("ingredient") val name: String? = null,
+)
--- app/src/main/java/com/jkm/recipeapp/feature/recipe/domain/RecipeModels.kt
+++ app/src/main/java/com/jkm/recipeapp/feature/recipe/domain/RecipeModels.kt
@@ -1,2 +1,33 @@
 package com.jkm.recipeapp.feature.recipe.domain
 
+// TODO: for the mappers, do  I want the toDomain or toDomainX?
+
+// TODO: decide on what you'd consider mandatory properties versus nice to have
+
+data class Recipe(
+    val title: String,
+    val description: String,
+    val thumbnailUrl: String,
+    val thumbnailAlt: String,
+    val details: RecipeDetail, // JKM - at what point do we say so much is missing that we should bin it?
+    val ingredients: Ingredients,
+)
+
+// JKM - would pairs or some other modeling be better? Not showing values without their label would be good.
+data class RecipeDetail(
+    val amountLabel: String? = null,
+    val amountNumber: String? = null,
+    val prepLabel: String? = null,
+    val prepNote: String? = null,
+    val prepTime: String? = null,
+    val cookingLabel: String? = null,
+    val cookingTime: String? = null,
+    val cookTimeMinutes: String? = null,
+    val prepTimeMinutes: String? = null,
+)
+typealias Ingredients = List<Ingredient>
+
+@JvmInline
+value class Ingredient(
+    val name: String,
+)
