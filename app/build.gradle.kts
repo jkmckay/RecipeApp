4616aea6e3818ec6349c9d7d1e7ec88692b0c262
Jasmine Tsan <jasminetsan@Jasmines-MacBook-Pro.local>
2026-03-09 12:58:04 +1100

    update sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.jkm.recipeapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jkm.recipeapp"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
